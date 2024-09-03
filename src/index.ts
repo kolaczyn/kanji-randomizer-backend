c6f4453d80257cfea462e0cafc054573db97254d
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2024-09-03 18:44:56 +0200

    setup basic data

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,48 @@
 import express from 'express'
+import { kanjiN5 } from './data/kanjiN5'
+import { kanjiN1 } from './data/kanjiN1'
+import { kanjiN2 } from './data/kanjiN2'
+import { kanjiN3 } from './data/kanjiN3'
+import { kanjiN4 } from './data/kanjiN4'
+import { KanjiList } from './types'
 
 const app = express()
 
-app.get('/', (_req, res) => {
-  res.send('Hello World')
+const levelToKanji = (level: string): KanjiList | null => {
+  switch (level) {
+    case 'N1':
+      return kanjiN1
+    case 'N2':
+      return kanjiN2
+    case 'N3':
+      return kanjiN3
+    case 'N4':
+      return kanjiN4
+    case 'N5':
+      return kanjiN5
+    default:
+      return null
+  }
+}
+
+app.get('/kanji/:level', (req, res) => {
+  const level = req.params.level.toUpperCase()
+  const kanji = levelToKanji(level)
+  if (kanji) {
+    res.json(kanji)
+  } else {
+    res.status(404).send('Not Found')
+  }
+})
+
+app.get('/kanji/:level/length', (req, res) => {
+  const level = req.params.level.toUpperCase()
+  const kanji = levelToKanji(level)
+  if (kanji) {
+    res.json({ length: kanji.length })
+  } else {
+    res.status(404).send('Not Found')
+  }
 })
 
 app.listen(3000)
---
+++ src/types.ts
@@ -0,0 +1,3 @@
+export type Level = 'n5' | 'n4' | 'n3' | 'n2' | 'n1'
+
+export type KanjiList = [string, string][]
