15517649a243c1d6e34c0d562de3bd65ec48f964
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2024-09-10 11:05:54 +0200

    remove unused endpoint

--- src/server.ts
+++ src/server.ts
@@ -47,22 +47,6 @@ app.get('/v2/decks/join', (req, res) => {
   res.json(response)
 })
 
-app.get('/v2/decks/:id', (req, res) => {
-  const { id } = req.params
-  if (!id) return res.status(400).send('Id not provided')
-
-  const deck = allDecksGeneric[id]
-  if (!deck) return res.status(404).send('Deck not found')
-
-  const cardsDto = deckDbToCardsDto(deck.deck)
-  const response: DeckDto = {
-    id: deck.id,
-    title: deck.title,
-    deck: cardsDto,
-  }
-  res.json(response)
-})
-
 app.get('/v2/home/tiles', (_req, res) => {
   const response: HomeTilesDto = { tiles: getHomeTiles() }
   res.json(response)
